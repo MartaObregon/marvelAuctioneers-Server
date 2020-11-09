fe640011ad21cb25f3772fd5404fff7f600d32e9
Marta Obregon <marta.garcia.obregon@.com>
2020-11-09 19:08:50 +0000

    saleList, saleDetail

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -17,4 +17,19 @@ router.get('/search', (req, res)=>{
     })
 })
 
+router.get('/detail/:id', (req, res)=>{
+  let id = req.params.id
+  console.log(req.params.id)
+    SaleModel.findById(id)
+      .then((sale)=>{
+        res.status(200).json(sale)
+      })
+      .catch((err)=>{
+        res.status(500).json({
+          error: 'Something went wrong',
+          message: err,
+        })
+      })
+})
+
 module.exports = router
